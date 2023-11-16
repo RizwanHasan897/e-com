d74147feb1262af74c0bd9248de79a0cadee15ca
Rizwan Hasan <rizwanhasan897@gmail.com>
2023-11-16 12:31:44 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Admin from './Componants/Admin';
+
+function fileInput(event) {
+  const fileInput = event.target;
+  const file = fileInput.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      const data = event.target.result;
+      loadFile(data);
+    };
+    reader.readAsText(file);
+  }
+}
+
+function loadFile(data) {
+  const container = document.querySelector('.table-container');
+
+  const dataRows = data.split('\r\n');
+  const finalArray = dataRows.map((row) => row.split(',').map((col) => col.trim()));
+
+  const table = document.createElement('table');
+
+  finalArray.forEach((rowData) => {
+    const row = document.createElement('tr');
+    rowData.forEach((cellData) => {
+      if (cellData !== '') {
+        const cell = document.createElement('td');
+        cell.textContent = cellData;
+        row.appendChild(cell);
+      }
+    });
+
+    if (row.children.length > 0) {
+      table.appendChild(row);
+    }
+  });
+
+  container.innerHTML = '';
+  container.appendChild(table);
+
+  localStorage.setItem('tableData', JSON.stringify(finalArray));
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Admin fileInput={fileInput} loadFile={loadFile} />
+      <User />
     </div>
   );
 }
